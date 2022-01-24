cbc59cf53c6dd8dfb172eacc2226de3c4c8d9862
Arfan A Khan <73901113+ArfanGits@users.noreply.github.com>
2022-01-24 12:54:46 +0600

    web.php > routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SliderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,83 +14,85 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('template.index');
-});
+Route::resource('sliders',SliderController::class);
+
+// Route::get('/', function () {
+//     return view('template.index');
+// });
 
 
-Route::get('/404', function () {
-    return view('template.404');
-});
+// Route::get('/404', function () {
+//     return view('template.404');
+// });
 
-Route::get('/about', function () {
-    return view('template.about');
-});
+// Route::get('/about', function () {
+//     return view('template.about');
+// });
 
-Route::get('/blog-details', function () {
-    return view('template.blog-details');
-});
+// Route::get('/blog-details', function () {
+//     return view('template.blog-details');
+// });
 
-Route::get('/blog', function () {
-    return view('template.blog');
-});
+// Route::get('/blog', function () {
+//     return view('template.blog');
+// });
 
-Route::get('/cart', function () {
-    return view('template.cart');
-});
+// Route::get('/cart', function () {
+//     return view('template.cart');
+// });
 
-Route::get('/checkout', function () {
-    return view('template.checkout');
-});
+// Route::get('/checkout', function () {
+//     return view('template.checkout');
+// });
 
-Route::get('/contact', function () {
-    return view('template.contact');
-});
+// Route::get('/contact', function () {
+//     return view('template.contact');
+// });
 
-Route::get('/faq', function () {
-    return view('template.faq');
-});
+// Route::get('/faq', function () {
+//     return view('template.faq');
+// });
 
-Route::get('/forget-password', function () {
-    return view('template.forget-password');
-});
+// Route::get('/forget-password', function () {
+//     return view('template.forget-password');
+// });
 
-Route::get('/index-2', function () {
-    return view('template.index-2');
-});
+// Route::get('/index-2', function () {
+//     return view('template.index-2');
+// });
 
-Route::get('/index', function () {
-    return view('template.index');
-});
+// Route::get('/index', function () {
+//     return view('template.index');
+// });
 
-Route::get('/login', function () {
-    return view('template.login');
-});
+// Route::get('/login', function () {
+//     return view('template.login');
+// });
 
-Route::get('/my-account', function () {
-    return view('template.my-account');
-});
+// Route::get('/my-account', function () {
+//     return view('template.my-account');
+// });
 
-Route::get('/privacy-policy', function () {
-    return view('template.privacy-policy');
-});
+// Route::get('/privacy-policy', function () {
+//     return view('template.privacy-policy');
+// });
 
-Route::get('/product-details', function () {
-    return view('template.product-details');
-});
+// Route::get('/product-details', function () {
+//     return view('template.product-details');
+// });
 
-Route::get('/register', function () {
-    return view('template.register');
-});
+// Route::get('/register', function () {
+//     return view('template.register');
+// });
 
-Route::get('/services', function () {
-    return view('template.services');
-});
+// Route::get('/services', function () {
+//     return view('template.services');
+// });
 
-Route::get('/tracking', function () {
-    return view('template.tracking');
-});
+// Route::get('/tracking', function () {
+//     return view('template.tracking');
+// });
 
-Route::get('/wishlist', function () {
-    return view('template.wishlist');
-});
+// Route::get('/wishlist', function () {
+//     return view('template.wishlist');
+// });
